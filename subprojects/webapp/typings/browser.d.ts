bd6a0293c7e5f6932de3e552bf7d22c587f52ab2
Niklas Walter <walter.niklas@gmail.com>
2016-03-17 15:20:25 +0100

    Remove generated files

--- subprojects/webapp/typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser\ambient\bootstrap\bootstrap.d.ts" />
-/// <reference path="browser\ambient\es6-shim\es6-shim.d.ts" />
-/// <reference path="browser\ambient\jquery\jquery.d.ts" />
-/// <reference path="browser\ambient\moment-node\moment-node.d.ts" />
-/// <reference path="browser\ambient\moment\moment.d.ts" />
-/// <reference path="browser\ambient\string_score\string_score.d.ts" />
--- subprojects/webapp/typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="main\ambient\bootstrap\bootstrap.d.ts" />
-/// <reference path="main\ambient\es6-shim\es6-shim.d.ts" />
-/// <reference path="main\ambient\jquery\jquery.d.ts" />
-/// <reference path="main\ambient\moment-node\moment-node.d.ts" />
-/// <reference path="main\ambient\moment\moment.d.ts" />
-/// <reference path="main\ambient\string_score\string_score.d.ts" />
