47c205012262552392dc45f0e363f35df1d9b126
Niklas Walter <walter.niklas@gmail.com>
2017-02-12 19:38:17 +0100

    Change jpa persistence to eclipselink because of https://hibernate.atlassian.net/browse/HHH-5694

---
+++ src/main/java/com/github/twostone/leaderboard/PersistanceConfiguration.java
@@ -0,0 +1,40 @@
+package com.github.twostone.leaderboard;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import com.google.common.collect.Maps;
+
+@Configuration
+public class PersistanceConfiguration extends JpaBaseConfiguration {
+
+  protected PersistanceConfiguration(DataSource dataSource, JpaProperties properties,
+      ObjectProvider<JtaTransactionManager> jtaTransactionManager,
+      ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+    super(dataSource, properties, jtaTransactionManager, transactionManagerCustomizers);
+  }
+
+  @Override
+  protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+    return new EclipseLinkJpaVendorAdapter();
+  }
+
+  @Override
+  protected Map<String, Object> getVendorProperties() {
+    Map<String, Object> properties = Maps.newHashMap();
+    properties.put("eclipselink.weaving", "false");
+    return properties;
+  }
+
+}
